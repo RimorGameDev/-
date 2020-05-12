8737900a2c01d3e5baf82c489d97233226c06661
Rimor <59154276+RimorGameDev@users.noreply.github.com>
2020-05-12 21:01:22 +0700

    Add files via upload

---
+++ Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace СайтСистема
+{
+    class Profile
+    {
+        public string name;
+        public string email;
+        public int age;
+        public bool isAdmin;
+
+        public void setAll(string _name, string _email, int _age)
+        {
+            name = _email;
+            email = _email;
+            age = _age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace СайтСистема
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! Если у тебя уже есть аккаунт, то напиши ВОЙТИ, если нет - ЗАРЕГИСТРИРОВАТЬСЯ");
+            string answerReg = Console.ReadLine();
+            if (answerReg == "Войти" || answerReg == "войти")
+            {
+                Console.WriteLine("Введите имя: ");
+                string name = Console.ReadLine();
+                
+                if (name != "Valera")
+                {
+                    Console.WriteLine("Ошибка, введите правильное имя пользователя!");
+                }
+                if (name == "Valera")
+                {
+                    Console.WriteLine("Пароль: ");
+                    string passsword = Console.ReadLine();
+                    if (passsword != "12345")
+                    {
+                        Console.WriteLine("Введите правильный пароль!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Добро пожаловать на страницу, " + name);
+                    }
+                }
+            }
+            if (answerReg == "Зарегистрироваться")
+            {
+                
+            }
+            
+            
+        }
+    }
+}
